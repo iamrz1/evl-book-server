a13333b6ad6e91805a417c84957169e2751f3f41
iamrz1 <rezoan.tamal@gmail.com>
2020-04-16 02:10:47 +0600

    Added middleware for admin verification for url of type /api/admin/{endpoint}

--- auth/auth.go
+++ auth/auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/sessions"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -12,6 +13,8 @@ import (
 
 var (
 	Store *sessions.CookieStore
+	trueString              = "true"
+	falseString              = "false"
 )
 
 type Auth struct{}
@@ -39,6 +42,48 @@ func (*Auth) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.Handler
 		return
 	}
 	claimMap := token.Claims.(jwt.MapClaims)
+	for key, value := range claimMap {
+		r.Header.Add(key, fmt.Sprintf("%v", value))
+	}
+	next(w, r)
+	fmt.Printf("Execution time: %s \n", time.Now().Sub(t).String())
+}
+
+
+type Admin struct{}
+
+func (*Admin) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	t := time.Now()
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Need Bearer authorization! Generate token using your username and password here: http://localhost:<port>/api/login\n"))
+		return
+	}
+	token, err := jwt.Parse(strings.Split(authHeader, " ")[1], func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.App().Key), nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if !token.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("token not valid"))
+		return
+	}
+	claimMap := token.Claims.(jwt.MapClaims)
+
+	log.Println(claimMap[AdminKey])
+	log.Println()
+
+	if claimMap[AdminKey] == false{
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("token not valid for administrative work"))
+		return
+	}
 	for key, value := range claimMap {
 		r.Header.Add(key, fmt.Sprintf("%v", value))
 	}
